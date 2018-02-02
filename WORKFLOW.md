e5907f88462a037dcaa313f10e350ba9eb683deb
anasayubi <anasayubi7152@gmail.com>
2018-02-02 23:35:59 +0500

    add hashbang, cmd interface, send multiple emails

--- WORKFLOW.md
+++ WORKFLOW.md
@@ -1,6 +1,5 @@
 # Workflow
+- Activate the correct environment that has all the correct dependencies installed via `source activate email-conf-registry`
 - Adopt Readme Driven Development (RDD) as defined [here](http://tom.preston-werner.com/2010/08/23/readme-driven-development.html)
-- Use [`grip`](https://github.com/joeyespo/grip) to view the markdown file as it will be rendered on Github
-  - To start a brower with markdown of a file with grip do the following:
-    - `$ grip [name-of-file]`
-    - open `http://localhost:[post-number]` on browser
+- Use [`grip`](https://github.com/joeyespo/grip) to view the markdown file as it will be rendered on Github. To run grip do the following:
+  - `$ grip [name-of-file] -b` -> opens a browser window automatically
--- main.py
+++ main.py
@@ -1,20 +1,55 @@
+#!/usr/bin/env python
+
 import smtplib
+import argparse
+import sys
+
+# parse cmd arguments
+cmd_parser = argparse.ArgumentParser(description='Email confirmation registry')
+cmd_parser.add_argument('email', help='Email addresses to send to', nargs='+')
+cmd_parser.add_argument('-c', metavar="CONTENT", dest="content", help='Email content to be sent. Defaults to stdin if omitted', nargs=1, type=argparse.FileType('r'), default=sys.stdin)
+args = cmd_parser.parse_args()
+print(args)
+
+# in some cases args.content is returned as single element list
+#   if it is then simply restore the element in args.content (remove list)
+if isinstance(args.content, list):
+    args.content = args.content[0]
+
+for email in args.email:
+    if '@' not in email and args.content.name != "<stdin>":
+        print('Email "' +  email + '" is invalid. Only one argument to -c is allowed')
+        exit()
+    if '@' not in email:
+        print('Email "' +  email + '" is invalid')
+        exit()
 
 # Set constants
 CONST_GMAIL_SMTP = 'smtp.gmail.com'
 TLS_PORT = 587
 CLIENT_EMAIL = "anasayubi7152@gmail.com"
-CLIENT_PASSWORD = '' 
+CLIENT_PASSWORD = ''
+CONTENT = '' 
+
+# print message if input will be taken from stdin
+if args.content.name == "<stdin>":
+    print("Message Content:")
+
+# grab content (works for file or stdin)
+CONTENT = args.content.read()
+
+#print(CONTENT) 
 
 # Get password
 email_password_file = open("email_password.txt", "r")
 CLIENT_PASSWORD = email_password_file.readline()
 
 # Send email
-s = smtplib.SMTP(CONST_GMAIL_SMTP, TLS_PORT)
-print(s.ehlo())
-print(s.starttls())
-print(s.ehlo())
-print(s.login(CLIENT_EMAIL, CLIENT_PASSWORD))
-print(s.sendmail(CLIENT_EMAIL, 'anasayubi@hotmail.com', "Subject: hello\nhey, how are you?"))
-print(s.quit())
+for email in args.email:
+    s = smtplib.SMTP(CONST_GMAIL_SMTP, TLS_PORT)
+    print(s.ehlo())
+    print(s.starttls())
+    print(s.ehlo())
+    print(s.login(CLIENT_EMAIL, CLIENT_PASSWORD))
+    print(s.sendmail(CLIENT_EMAIL, email, CONTENT))
+    print(s.quit())
